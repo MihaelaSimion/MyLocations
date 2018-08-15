397dc1767db4b4a6baf016516d977350ca6f5d3b
Mihaela Simion <mihaelasimion2016@gmail.com>
2018-08-15 11:17:47 +0300

    Initial Commit

---
+++ MyLocations/CurrentLocationViewController.swift
@@ -0,0 +1,82 @@
+//
+//  FirstViewController.swift
+//  MyLocations
+//
+//  Created by Mihaela Simion on 8/14/18.
+//  Copyright © 2018 Mihaela Simion. All rights reserved.
+//
+
+import UIKit
+import CoreLocation
+
+class CurrentLocationViewController: UIViewController {
+    
+    let locationManager = CLLocationManager()
+    var location: CLLocation?
+
+    @IBOutlet weak var messageLabel: UILabel!
+    @IBOutlet weak var latitudeLabel: UILabel!
+    @IBOutlet weak var longituteLabel: UILabel!
+    @IBOutlet weak var addressLabel: UILabel!
+    @IBOutlet weak var tagButton: UIButton!
+    @IBOutlet weak var getButton: UIButton!
+    
+    @IBAction func getLocation() {
+        let authStatus = CLLocationManager.authorizationStatus()
+        if authStatus == .notDetermined {
+            locationManager.requestWhenInUseAuthorization()
+            return
+        }
+        
+        if authStatus == .denied || authStatus == .restricted {
+            showLocationServicesDeniedAlert()
+            return
+        }
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyNearestTenMeters
+        locationManager.startUpdatingLocation()
+        
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        updateLabel()
+    }
+    
+    func showLocationServicesDeniedAlert() {
+        let alert = UIAlertController(title: "Location Services Disabled", message: "Please enable location services for this app in Settings.", preferredStyle: .alert)
+        let okAction = UIAlertAction(title: "OK", style: .default, handler: nil)
+        alert.addAction(okAction)
+        present(alert, animated: true, completion: nil)
+    }
+    
+    func updateLabel() {
+        if let location = location {
+            latitudeLabel.text = String(format: "%.8f", location.coordinate.latitude)
+            longituteLabel.text = String(format: "%.8f", location.coordinate.longitude)
+            tagButton.isHidden = false
+            addressLabel.text = ""
+            messageLabel.text = ""
+        } else {
+            latitudeLabel.text = ""
+            longituteLabel.text = ""
+            addressLabel.text = ""
+            tagButton.isHidden = true
+            messageLabel.text = "Tap 'Get My Location' to Start"
+        }
+    }
+}
+
+extension CurrentLocationViewController: CLLocationManagerDelegate {
+    // MARK: - CLLocationManagerDelegate
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        print("didFailWithError \(error)")
+    }
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        let newLocation = locations.last!
+        print("didUpdateLocations \(newLocation)")
+        location = newLocation
+        updateLabel()
+    }
+}
+
---
+++ MyLocations/SecondViewController.swift
@@ -0,0 +1,17 @@
+//
+//  SecondViewController.swift
+//  MyLocations
+//
+//  Created by Mihaela Simion on 8/14/18.
+//  Copyright © 2018 Mihaela Simion. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
