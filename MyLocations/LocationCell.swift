3165402d75be885b7b78fb7ecdfd4ffdc7eb59a1
Mihaela Simion <mihaelasimion2016@gmail.com>
2018-08-19 18:37:31 +0300

    Custom cell

---
+++ MyLocations/LocationCell.swift
@@ -0,0 +1,39 @@
+//
+//  LocationCell.swift
+//  MyLocations
+//
+//  Created by Mihaela Simion on 8/19/18.
+//  Copyright © 2018 Mihaela Simion. All rights reserved.
+//
+
+import UIKit
+
+class LocationCell: UITableViewCell {
+    
+    @IBOutlet weak var descriptionLabel: UILabel!
+    @IBOutlet weak var addressLabel: UILabel!
+    
+    func configure(for location: Location) {
+        if location.locationDescription.isEmpty {
+            descriptionLabel.text = "(No description)"
+        } else {
+            descriptionLabel.text = location.locationDescription
+        }
+        
+        if let placemark = location.placemark {
+            var text = ""
+            if let s = placemark.subThoroughfare {
+                text += s + " "
+            }
+            if let s = placemark.thoroughfare {
+                text += s + ", "
+            }
+            if let s = placemark.locality {
+                text += s
+            }
+            addressLabel.text = text
+        } else {
+            addressLabel.text = String(format: "Lat: %.8f, Long: %.8f", location.latitude, location.longitute)
+        }
+    }
+}
